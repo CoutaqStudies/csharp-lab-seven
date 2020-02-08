6797d64fe2695b9c9dcbb7d171e391dbfe87c4bc
Michael Melikov <coutaq@gmail.com>
2020-02-09 01:53:52 +0300

    Add project files.

---
+++ LabSeven/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LabOne;
+
+namespace LabFive
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            List<GeographicalUnit> countries = new List<GeographicalUnit>();
+            List<LogEntry> log = new List<LogEntry>();
+            ConsoleApp.Execute(countries, log);
+        }
+    }
+}
---
+++ LabSeven/Sorter.cs
@@ -0,0 +1,70 @@
+﻿using LabOne;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LabSeven
+{
+    class Sorter
+    {
+        public static Boolean isListSorted(List<int> list)
+        {
+            for(int i = 0; i < list.Count-1; i++)
+            {
+                if((list[i]>list[i+1]))
+                    return false;
+            }
+            return true;
+        }
+        public static List<GeographicalUnit> SelectionSort(List<GeographicalUnit> list){
+            List<GeographicalUnit> finalList = new List<GeographicalUnit>();
+            while (list.Count>0)
+            {
+                int smallestPop = list.Min(country => country.getPopulation());
+                finalList.Add(list.Find(country => country.getPopulation() == smallestPop));
+                list.Remove(list.Find(country => country.getPopulation() == smallestPop));
+            }
+            return finalList;
+        }
+        public static List<int> InsertionSort(List<int> list)
+        {
+            List<int> finalList = new List<int>();
+            while (!isListSorted(finalList) || finalList.Count<list.Count)
+            {
+                if(finalList.Count<list.Count)
+                finalList.Add(list[finalList.Count]);
+
+                for (int i = 0; i < finalList.Count-1; i++)
+                {
+                    if (finalList[i] > finalList[i + 1])
+                    {
+                        finalList.Insert(i, finalList[i+1]);
+                        finalList.RemoveAt(i+2);
+                    }
+                }
+            }
+            return finalList;
+        }
+        // TODO: 
+        /*public static List<int> InsertionSort(List<int> list)
+        {
+            List<int> finalList = new List<int>();
+            while (!isListSorted(finalList) || finalList.Count < list.Count)
+            {
+                if (finalList.Count < list.Count)
+                    finalList.Add(list[finalList.Count]);
+
+                for (int i = 0; i < finalList.Count - 1; i++)
+                {
+                    if (finalList[i] > finalList[i + 1])
+                    {
+                        finalList.Insert(i, finalList[i + 1]);
+                        finalList.RemoveAt(i + 2);
+                    }
+                }
+            }
+            return finalList;
+        }*/
+    }
+}
